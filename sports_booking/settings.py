e41410420bcc4d18f3e0ae5af4a8b2a42ab94d02
sa <saifhathout22@gmail.com>
2026-06-22 18:53:07 +0300

    revert sql

--- sports_booking/settings.py
+++ sports_booking/settings.py
@@ -64,13 +64,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sports_booking.wsgi.application'
 
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://...',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 AUTH_USER_MODEL = 'accounts.User'
