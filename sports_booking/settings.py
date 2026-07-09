2b4a289cc5f55e2fbca33a360212d99aeb18d2b7
sa <saifhathout22@gmail.com>
2026-07-09 16:57:15 +0300

    fix screen 6

--- sports_booking/settings.py
+++ sports_booking/settings.py
@@ -98,14 +98,6 @@ TEMPLATES = [
 
 # sports_booking/settings.py
 
-import cloudinary
-import cloudinary.uploader
-
-cloudinary.config(
-    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    api_key=os.environ.get('CLOUDINARY_API_KEY'),
-    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
-)
 
 # ✅ ========== AUTH ==========
 AUTH_USER_MODEL = 'accounts.User'
